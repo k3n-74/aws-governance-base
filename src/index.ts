8f557971d52c16c2f914afa0f4ffd2c569b419f5
k3n-74 <dev.kentakehara@gmail.com>
2022-11-03 18:40:06 +0900

    aws sso で 全アカウントのエイリアス取得

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,63 @@
 import yargs from "yargs";
 import * as yaml from "js-yaml";
 import { readFileSync } from "fs";
+import { fromSSO } from "@aws-sdk/credential-providers";
+import { IAMClient, ListAccountAliasesCommand } from "@aws-sdk/client-iam";
 
-const args = yargs
-  .options({
-    "config-file": {
-      describe: "config file path",
-      alias: "c",
-      type: "string",
-      require: true,
-    },
-  })
-  .parseSync();
+const getAccountAlias = async (
+  profile: string,
+  region: string
+): Promise<string | undefined> => {
+  const iamClient = new IAMClient({
+    credentials: fromSSO({
+      profile: profile,
+    }),
+    region: region,
+  });
 
-const config = yaml.load(readFileSync(args["config-file"], "utf-8"));
+  const data = await iamClient.send(
+    new ListAccountAliasesCommand({ MaxItems: 1 })
+  );
+  return data.AccountAliases?.[0];
+};
 
-console.log(args["config-file"]);
+const main = async () => {
+  try {
+    const args = yargs
+      .options({
+        "config-file": {
+          describe: "config file path",
+          alias: "c",
+          type: "string",
+          require: true,
+        },
+      })
+      .parseSync();
 
-console.log(yaml.dump(config));
+    const config: any = yaml.load(readFileSync(args["config-file"], "utf-8"));
+
+    const BASE_REGION = config["General"]["BaseRegion"];
+
+    console.log(args["config-file"]);
+    console.log(BASE_REGION);
+
+    // console.log(yaml.dump(config));
+
+    const profileList = config["General"]["Profiles"];
+    console.log(profileList);
+    for (const id of Object.keys(profileList)) {
+      console.log(id, profileList[id]);
+      const accountAliasName = await getAccountAlias(
+        profileList[id],
+        BASE_REGION
+      );
+      console.log(accountAliasName);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+(async () => {
+  await main();
+})();
