c22f0396544752757329df6d7e667652e5324411
k3n-74 <dev.kentakehara@gmail.com>
2022-11-03 17:29:21 +0900

    構成ファイルの読み込み

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import yargs from "yargs";
+import * as yaml from "js-yaml";
+import { readFileSync } from "fs";
+
+const args = yargs
+  .options({
+    "config-file": {
+      describe: "config file path",
+      alias: "c",
+      type: "string",
+      require: true,
+    },
+  })
+  .parseSync();
+
+const config = yaml.load(readFileSync(args["config-file"], "utf-8"));
+
+console.log(args["config-file"]);
+
+console.log(yaml.dump(config));
