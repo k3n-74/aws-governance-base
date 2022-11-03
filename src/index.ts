61e1542f8dc134372d3db439f2c3603ad938b973
k3n-74 <dev.kentakehara@gmail.com>
2022-11-04 01:14:26 +0900

    改良

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,39 @@ import * as yaml from "js-yaml";
 import { readFileSync } from "fs";
 import { fromSSO } from "@aws-sdk/credential-providers";
 import { IAMClient, ListAccountAliasesCommand } from "@aws-sdk/client-iam";
+import { CredentialProvider } from "@aws-sdk/types";
 
-const getAccountAlias = async (
-  profile: string,
+type AwsGovBaseConfig = {
+  General: {
+    BaseRegion: string;
+    Profiles: Record<string, string>;
+  };
+  Structure: {
+    Jump: {
+      id: string;
+    };
+    Audit: {
+      id: string;
+    };
+    Guests: [{ id: string }];
+  };
+};
+
+const getSsoCredential = async (
+  awsAccountId: string,
+  profiles: AwsGovBaseConfig["General"]["Profiles"]
+): Promise<CredentialProvider> => {
+  const profileName = profiles[awsAccountId];
+  if (profileName == null) throw Error("profile name is undefined.");
+  return fromSSO({ profile: profileName });
+};
+
+const getAwsAccountAlias = async (
+  credential: CredentialProvider,
   region: string
 ): Promise<string | undefined> => {
   const iamClient = new IAMClient({
-    credentials: fromSSO({
-      profile: profile,
-    }),
+    credentials: credential,
     region: region,
   });
 
@@ -19,6 +43,18 @@ const getAccountAlias = async (
     new ListAccountAliasesCommand({ MaxItems: 1 })
   );
   return data.AccountAliases?.[0];
+};
+
+const printAllAwsAccountAlias = async (
+  profiles: AwsGovBaseConfig["General"]["Profiles"],
+  baseRegion: AwsGovBaseConfig["General"]["BaseRegion"]
+) => {
+  // 全AWSアカウントのエイリアスを出力
+  for (const awsAccountId of Object.keys(profiles)) {
+    const credential = await getSsoCredential(awsAccountId, profiles);
+    const accountAliasName = await getAwsAccountAlias(credential, baseRegion);
+    console.log(awsAccountId, accountAliasName);
+  }
 };
 
 const main = async () => {
@@ -34,25 +70,25 @@ const main = async () => {
       })
       .parseSync();
 
-    const config: any = yaml.load(readFileSync(args["config-file"], "utf-8"));
+    const awsGovBaseConfig = yaml.load(
+      readFileSync(args["config-file"], "utf-8")
+    ) as AwsGovBaseConfig;
 
-    const BASE_REGION = config["General"]["BaseRegion"];
+    const BASE_REGION = awsGovBaseConfig.General.BaseRegion;
+    const PROFILES = awsGovBaseConfig.General.Profiles;
+    const STRUCTURE = awsGovBaseConfig.Structure;
 
     console.log(args["config-file"]);
     console.log(BASE_REGION);
+    console.log(PROFILES);
 
     // console.log(yaml.dump(config));
 
-    const profileList = config["General"]["Profiles"];
-    console.log(profileList);
-    for (const id of Object.keys(profileList)) {
-      console.log(id, profileList[id]);
-      const accountAliasName = await getAccountAlias(
-        profileList[id],
-        BASE_REGION
-      );
-      console.log(accountAliasName);
-    }
+    // 全AWSアカウントのエイリアスを出力
+    await printAllAwsAccountAlias(PROFILES, BASE_REGION);
+    console.log("bye");
+    // // Jump
+    // await setupJump(PROFILES, BASE_REGION, STRUCTURE);
   } catch (e) {
     console.error(e);
   }
