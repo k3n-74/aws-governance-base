d89d160172910b797542e5d1df35398bd9602e34
k3n-74 <dev.kentakehara@gmail.com>
2022-11-25 17:21:53 +0900

    devops guru の全AWSアカウント、全リージョンデプロイ

--- src/feature/devops-guru.ts
+++ src/feature/devops-guru.ts
@@ -9,7 +9,9 @@ import {
   isSetupTargetRegion,
   deploy,
   DeployFuncInput,
+  listAvailableRegions,
 } from "../util";
+import * as throat from "throat";
 import { logger } from "../logger";
 
 export class DevOpsGuruFeature {
@@ -19,18 +21,65 @@ export class DevOpsGuruFeature {
     // セットアップ対象外の機能だったら何もしないで終了
     if (!isSetupTargetFeature(this.featureNameList)) return;
 
-    // Guest
-    for (const awsAccount of C.i.structure.Guests) {
-      await deploy({
-        awsAccountId: awsAccount.id,
-        region: C.i.general.BaseRegion,
-        stacks: [
-          {
-            templateName: "devops-guru",
-            templateFilePath: `${__dirname}/../../cfn/devops-guru/devops-guru.yaml`,
-          },
-        ],
-      });
+    // 全AWSアカウントのリストを作成する
+    // GuestアカウントのリストからAWS Account IDだけのリストを作成する
+    const allAwsAccountIds: string[] = C.i.structure.Guests.map((account) => {
+      return account.id;
+    });
+    allAwsAccountIds.push(C.i.structure.Jump.id);
+    allAwsAccountIds.push(C.i.structure.Audit.id);
+
+    // https://www.aws-services.info/devops-guru.html
+    // https://aws.amazon.com/devops-guru/faqs/
+    const SUPPORTED_REGIONS_DEVOPS_GURU = [
+      "ap-northeast-1", //Tokyo
+      "ap-northeast-2", //Seoul
+      "ap-south-1", //Mumbai
+      "ap-southeast-1", //Singapore
+      "ap-southeast-2", //Sydney
+      "ca-central-1", //Central
+      "eu-central-1", //Frankfurt
+      "eu-north-1", //Stockholm
+      "eu-west-1", //Ireland
+      "eu-west-2", //London
+      "eu-west-3", //Paris
+      "sa-east-1", //Sao Paulo
+      "us-east-1", //N. Virginia
+      "us-east-2", //Ohio
+      "us-west-1", //N. California
+      "us-west-2", //Oregon
+    ];
+
+    // 全AWSアカウントにスタックをデプロイ
+    for (const awsAccountId of allAwsAccountIds) {
+      // TODO: listAvailableRegions関数の実行要否について、デプロイ対象のAWSアカウント、リージョンなのかを確認する機能を追加する
+      const availableRegions = await listAvailableRegions(awsAccountId);
+
+      // 全リージョン分の引数のリストを作成する
+      const deployFuncInputList: DeployFuncInput[] = [];
+      for (const region of availableRegions) {
+        // サポート対象のリージョンにのみデプロイする
+        if (SUPPORTED_REGIONS_DEVOPS_GURU.includes(region)) {
+          deployFuncInputList.push({
+            awsAccountId: awsAccountId,
+            region: region,
+            stacks: [
+              {
+                templateName: "devops-guru",
+                templateFilePath: `${__dirname}/../../cfn/devops-guru/devops-guru.yaml`,
+              },
+            ],
+          });
+        }
+      }
+      // 最大9並列の範囲内で全リージョンに対して一斉にdeployを実行
+      const res = await Promise.all(
+        deployFuncInputList.map(
+          throat.default(9, (deployFuncInput) => {
+            return deploy(deployFuncInput);
+          })
+        )
+      );
     }
   };
 }
